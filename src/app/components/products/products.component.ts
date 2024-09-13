91eb522856ff8e5a8ff248abd6e495d2db004ef5
user <artem77timoha77@gmail.com>
2024-09-13 13:47:45 +0600

    р

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,63 +1,123 @@
-import {Component, inject} from '@angular/core';
-import {ProductCardComponent} from "../product-card/product-card.component";
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {ProductsService} from "../../services/products.service";
 import {PaginatorComponent} from "../paginator/paginator/paginator.component";
 import {PageInfo, ProductType} from "../../interfaces/type";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 import {NgForOf} from "@angular/common";
 import {SpinnerComponent} from "../spinner/spinner.component";
-import {Router} from "@angular/router";
-
+import {ActivatedRoute, Router} from "@angular/router";
+import {PageEvent} from "@angular/material/paginator";
+import {ProductCardComponent} from "../product-card/product-card.component";
+import {CategoriesService} from "../../services/categories.service";
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [
-    ProductCardComponent,
     PaginatorComponent,
     MatGridList,
     NgForOf,
     MatGridTile,
     SpinnerComponent,
+    ProductCardComponent,
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
-  products: ProductType[] = []; // Массив для хранения продуктов
-  pageInfo?: PageInfo; // Делаем свойство необязательным
-  currentPage = 1; // Текущая страница
-  loadingProducts: boolean = false
+  products: ProductType[] = [];
+  pageInfo?: PageInfo;
+  loadingProducts = false;
   private router = inject(Router)
+  private rout = inject(ActivatedRoute)
+  categoriesService = inject(CategoriesService)
+  categoryName: string = ''
 
+  idCategory = signal<string>('');
 
   constructor(private productsService: ProductsService) {
   }
 
   ngOnInit() {
-    this.loadProducts(this.currentPage); // Загружаем продукты при инициализации компонента
+    this.rout.params.subscribe(params => {
+      this.idCategory.set(params['id'] || '');
+
+
+      this.loadProductsForCategoryOrDefault(this.idCategory(), 1);
+
+    });
+
+
   }
 
-  loadProducts(page: number) {
-    this.loadingProducts = true
-    this.productsService.getProducts(page).subscribe({
-      next: (response) => {
-        this.products = response.products; // Сохраняем полученные продукты
-        this.pageInfo = response.pageInfo; // Сохраняем информацию о страницах
-      },
+  loadCategoryName(idCategory: string) {
+    this.categoriesService.getCategories().subscribe({
+      next: (res => {
+        const foundCategory = res.find(item => item.ID === idCategory);
+        if (foundCategory) {
+          this.categoryName = foundCategory.name;
+        } else {
+          console.error('Категория не найдена');
+        }
+      }),
       error: (error: Error) => {
-        console.error('Ошибка при загрузке продуктов:', error);
-      },
-      complete: () => {
-        this.loadingProducts = false
+        console.error('Ошибка при загрузке категорий:', error);
       }
     });
   }
 
-  onPageChange(event: { pageIndex: number }) {
-    this.currentPage = event.pageIndex; // Обновляем текущую страницу
-    this.loadProducts(this.currentPage); // Перезагружаем продукты для новой страницы
+  loadProductsForCategoryOrDefault(idCategory: string, page: number) {
+    if (idCategory) {
+      this.loadProductsCategory(idCategory, page);
+      this.loadCategoryName(idCategory)
+
+    } else {
+      this.loadProducts(page);
+    }
+  }
+
+  loadProducts(page: number) {
+    this.loadingProducts = true;
+    this.productsService.getProducts(page).subscribe({
+      next: (response) => {
+        this.products = response.products;
+        this.pageInfo = response.pageInfo;
+      },
+      error: (error: Error) => {
+        console.error('Ошибка при загрузке продуктов:', error);
+        this.loadingProducts = false;
+      },
+      complete: () => {
+        this.loadingProducts = false;
+      }
+    });
+  }
+
+  loadProductsCategory(idCategory: string, page: number) {
+    this.loadingProducts = true;
+
+    this.productsService.getProductsCategory(idCategory, page).subscribe({
+      next: (res => {
+        this.products = res.products
+        this.pageInfo = res.pageInfo
+      }),
+      error: (error: Error) => {
+        console.log(error);
+        this.loadingProducts = false;
+      },
+      complete: () => {
+        this.loadingProducts = false;
+      }
+    })
+  }
+
+  onPageChange(event: PageEvent) {
+    const nextPage = event.pageIndex + 1;
+
+    if (nextPage >= 1) {
+      this.loadProducts(nextPage);
+    }
   }
 
   navigateToProduct(productId: string) {
